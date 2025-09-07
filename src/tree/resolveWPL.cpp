993c5ca68d0c9a3b6cfc0277c3fe6727f68244e7
Achang <134355354+just-a-muggle@users.noreply.github.com>
2025-09-07 13:08:48 +0800

    add resolveWPL.cpp

--- src/tree/resolveWPL.cpp
+++ src/tree/resolveWPL.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,4 +10,42 @@ struct BiTNode{
     BiTNode(int x): weight(x), lChild(nullptr), rChild(nullptr){}
 };
 
-void 
+int WPL;
+
+void PreOrder(BiTNode *root, int level){
+    if(root == nullptr) return;
+    if(root ->lChild == nullptr) WPL += level * root -> weight;
+    PreOrder(root->lChild, level + 1);
+    PreOrder(root->rChild, level + 1);
+}
+
+int main()
+{
+     int n;
+    cout << ">> create binary_tree: " << endl << ">> inputs how many Nodes you need :";
+    cin >> n;
+
+    if (n == 0) {
+        cout << ">> Empty tree." << endl;
+        return 0;   
+    }   
+
+    vector<BiTNode*> nodes;
+    nodes.reserve(n); //为nodes动态数组分配至少n个元素空间
+
+    for(int i = 0; i < n; i++) nodes.push_back(new BiTNode(i));
+
+    for(int i = 0; i < n; i ++){ //构建的是完全二叉树
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n) nodes[i]->lChild = nodes[left];
+        if (right < n) nodes[i]->rChild = nodes[right];
+    }
+    PreOrder(nodes.at(0), 1);
+    cout << "WPL is :  "<< WPL;
+  
+    for (auto &node : nodes) delete node;
+    cout << endl << ">> input any key to exit: ";
+    cin.get();
+    cin.get();
+}
